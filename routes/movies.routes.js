7d18bad004b533f4dcbaef3c66fbe9752c503d3a
ndiaye47 <ndiayebabacar129@gmail.com>
2024-04-27 19:14:37 +0000

    derniers reglages

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,11 +1,12 @@
 module.exports = app => {
     app.use("/api/movies0", router);
-  };
-  const movies0 = require("../controllers/movies.controller.js");
-  let router = require("express").Router();
-  
+};
+
+    const movies0 = require("../controllers/movies.controller.js");
+    let router = require("express").Router();
+    
     // Routes pour la gestion des films
-    router.get("/movies0", movies0.findAllMovies);
-    router.post("/movies0", movies0.createMovie);
-    router.put("/movies0/:id", movies0.updateMovie);
-    router.delete("/movies0/:id", movies0.deleteMovie);
+    router.get("/", movies0.findAllMovies);
+    router.post("/", movies0.createMovie);
+    router.put("/:id", movies0.updateMovie);
+    router.delete("/:id", movies0.deleteMovie);
