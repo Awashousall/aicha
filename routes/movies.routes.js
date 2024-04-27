ea381ebe7061f9ffe89725a82e3b1f3787a35534
ndiaye47 <ndiayebabacar129@gmail.com>
2024-04-27 13:45:46 +0000

    changement 4

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,11 +1,10 @@
+module.exports = router;
 const express = require("express");
 const router = express.Router();
-const studentController = require("../controllers/movies.controller");
+const moviesController = require("../controllers/movies.controller");
 
 // Routes pour la gestion des films
-router.get("/movies", studentController.findAllMovies);
-router.post("/movies", studentController.addMovie);
-router.put("/movies/:id", studentController.updateMovie);
-router.delete("/movies/:id", studentController.deleteMovie);
-
-module.exports = router;
+router.get("/movies", moviesController.findAllMovies);
+router.post("/movies", moviesController.addMovie);
+router.put("/movies/:id", moviesController.updateMovie);
+router.delete("/movies/:id", moviesController.deleteMovie);
