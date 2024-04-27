16711b8f65476bd9aea607eb94fd5ccb00e214e7
ndiaye47 <ndiayebabacar129@gmail.com>
2024-04-27 14:59:51 +0000

    changem 10

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,12 +1,16 @@
 module.exports = app => {
-};
-const express = require("express");
-const router = express.Router(); // Initialisation du routeur express
-
-const moviesController = require("../controllers/movies.controller");
-
-// Routes pour la gestion des films
-router.get("/movies", moviesController.findAllMovies);
-router.post("/movies", moviesController.addMovie);
-router.put("/movies/:id", moviesController.updateMovie);
-router.delete("/movies/:id", moviesController.deleteMovie);
+    const express = require("express");
+    const router = express.Router(); // Initialisation du routeur express
+  
+    const moviesController = require("../controllers/movies.controller");
+  
+    // Routes pour la gestion des films
+    router.get("/movies", moviesController.findAllMovies);
+    router.post("/movies", moviesController.addMovie);
+    router.put("/movies/:id", moviesController.updateMovie);
+    router.delete("/movies/:id", moviesController.deleteMovie);
+  
+    // Montez le routeur sur l'application Express
+    app.use("/api", router);
+  };
+  
