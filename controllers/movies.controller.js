6fb0b5fce21318d3a47a53758c529d84e56660f8
ndiaye47 <ndiayebabacar129@gmail.com>
2024-04-27 15:08:59 +0000

    cnge

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,18 +1,16 @@
-const movies = require("../movies.json");
+const db = require("../models");
+const Movie = db.movies;
 
 // Récupérer tous les films depuis la base de données.
 exports.findAllMovies = (req, res) => {
-  res.status(200).json(movies);
+  Movie.find()
+    .then(movies => {
+      res.status(200).json(movies);
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message || "Une erreur s'est produite lors de la récupération des films." });
+    });
 };
-
-// routes/movies.routes.js
-
-const express = require("express");
-const router = express.Router();
-const Movie = require("../models/movies.model");
-
-const db = require("../models");
-const Movie = db.movies;
 
 // Créer et enregistrer un nouveau film
 exports.createMovie = (req, res) => {
@@ -42,7 +40,6 @@ exports.createMovie = (req, res) => {
     });
 };
 
-
 // Mettre à jour un film en fonction de l'ID dans la requête
 exports.updateMovie = (req, res) => {
   const id_movie = req.params.id;
@@ -57,8 +54,9 @@ exports.updateMovie = (req, res) => {
     .catch(err => {
       res.status(500).json({ message: `Erreur lors de la mise à jour du film avec l'ID ${id_movie}.` });
     });
+};
 
-    // Supprimer un film avec l'ID spécifié dans la requête
+// Supprimer un film avec l'ID spécifié dans la requête
 exports.deleteMovie = (req, res) => {
   const id_movie = req.params.id;
 
@@ -72,5 +70,4 @@ exports.deleteMovie = (req, res) => {
     .catch(err => {
       res.status(500).json({ message: `Erreur lors de la suppression du film avec l'ID ${id_movie}.` });
     });
-};
 };
